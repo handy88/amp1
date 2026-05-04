4da362a02b9a9c3d9f9d257636173bb6ee27bc74
handy88 <handy@itplus.com>
2026-05-04 16:58:43 +0700

    asd

--- app/route.jsx
+++ app/route.jsx
@@ -12,6 +12,8 @@ export async function GET(request, { params }) {
  
   <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
   <link rel="canonical" href="${currentUrl}">
+
+  <meta name="google-site-verification" content="lq0osuXFXuTPJvzJTzUOB7vW_tCMA0dAMNiDRiZ2Afw" />
   
   <!-- SEO: Meta Description fokus pada variasi game kasino dan keamanan -->
   <meta name="description" content="Daftar di situs live casino online terbaik 2026. Akses link login alternatif agen kasino resmi dengan game Baccarat, Roulette, Sicbo, dan Blackjack kualitas HD terlengkap.">
